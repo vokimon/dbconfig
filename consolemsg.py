686339260ed544331989f1c65c2695c61db9100f
David García Garzón <voki@canvoki.net>
2015-01-14 12:24:39 +0100

    consolemsg: Py2 compatibility

--- consolemsg.py
+++ consolemsg.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+def printstderr(*args) :
+	import sys
+	for arg in args:
+		sys.stderr.write(arg)
+	sys.stderr.write('\n')
 
 def color(color, message) :
 	return "\033[{0}m{1}\033[0m".format(color,message)
 
 def step(message) :
 	import sys
-	print(color('34;1', ":: "+message), file=sys.stderr)
+	printstderr(color('34;1', ":: "+message))
 
 def error(message) :
 	import sys
-	print(color('31;1', "Error: "+message), file=sys.stderr)
+	printstderr(color('31;1', "Error: "+message))
 
 def warn(message) :
 	import sys
-	print(color('33', "Atenció: "+message), file=sys.stderr)
+	printstderr(color('33', "Atenció: "+message))
 
 def fail(message, code=-1) :
 	error(message)
